f3b3e2f065acfdb66a67c1d166368f68c92015b2
susmitamenka <susmitaprasad73@gmail.com>
2025-02-28 13:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let strongText = document.querySelectorAll("strong")
 function highlight() {
     //Write your code here
+	for(let t of strongText){
+		t.style.color =  "rgb(0, 128, 0)";
+	}
 
 
 }
@@ -7,6 +11,10 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	for(let t of strongText){
+		t.style.color =  "rgb(0, 0, 0)";
+	}
+
 
     
 }
